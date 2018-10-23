b362c7f2c61afd5b5a863672579a447b8e04633d
camilocruzv <38144641+camilocruzv@users.noreply.github.com>
2018-10-22 22:50:52 -0500

    Adding numerical methods

---
+++ Numerical_Methods_C#/Punto Fijo/PuntoFijo.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using org.mariuszgromada.math.mxparser;
+
+namespace ValorAgregrado
+{
+    class PuntoFijo
+    {
+        static void Main(string[] args) {
+            string funcF, funcG;
+            double xa, tolerancia;
+            int iteraciones;
+
+            Console.WriteLine("Convenciones usadas: pi, sin(), cos(), tan(), e^, abs(), ^, sqrt(), ln()");
+            Console.WriteLine("Ingrese la función f(x) a ser evaluada (Notación: f(x) = <expresión>)");
+            funcF = Console.ReadLine();
+            Console.WriteLine("La función es: " + funcF);
+
+            Console.WriteLine("Ingrese la función g(x) a ser evaluada (Notación: g(x) = <expresión>)");
+            funcG = Console.ReadLine();
+            Console.WriteLine("La función es: " + funcG);
+
+            Console.WriteLine("Ingrese la aproximación inicial: ");
+            xa = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la tolerancia: ");
+            tolerancia = Double.Parse(Console.ReadLine());
+            while (tolerancia == 0) {
+                Console.WriteLine("La tolerancia debe ser diferente de 0, ingresela nuevamente: ");
+                tolerancia = Double.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Ingrese el número maximo de iteraciones: ");
+            iteraciones = int.Parse(Console.ReadLine());
+            while (iteraciones <= 0) {
+                Console.WriteLine("El número de iteraciones debe ser mayor a 0, ingrésela nuevamente: ");
+                iteraciones = int.Parse(Console.ReadLine());
+            }
+
+            metodoPuntoFijo(funcF, funcG, xa, tolerancia, iteraciones);
+        }
+
+        public static void metodoPuntoFijo (string funcF, string funcG, double xa, double tolerancia, int iteraciones) {
+            double fx, xn, errorAbs, errorRel;
+            int cont;
+
+            Function fx1 = new Function(funcF);
+            Function gx = new Function(funcG);
+
+            fx = fx1.calculate(xa);
+
+            cont = 0;
+
+            Console.WriteLine("iteración | xa | fx | error absoluto | error relativo");
+            Console.WriteLine(cont + "|" + xa + "|" + fx + "|" + "---" + "|" + "---");
+
+            errorAbs = tolerancia + 1;
+
+            while (fx != 0 && errorAbs > tolerancia && cont < iteraciones) {
+                xn = gx.calculate(xa);
+                fx = fx1.calculate(xn);
+
+                errorAbs = Math.Abs(xn - xa);
+                errorRel = errorAbs/xn;
+                xa = xn;
+                cont = cont + 1;
+                Console.WriteLine(cont + "|" + xa + "|" + fx + "|" + errorAbs + "|" + errorRel);
+            }
+
+            if (fx == 0) {
+                Console.WriteLine(xa + " es una raíz");
+            } else if (errorAbs < tolerancia) {
+                Console.WriteLine(xa + " se aproxima a una raiz de la función, con una tolerancia de: " + tolerancia);
+            } else {
+                Console.WriteLine("Excedió el número de iteraciones posible");
+            }  
+        }
+    }
+}   
---
+++ Numerical_Methods_C#/Regla Falsa/ReglaFalsa.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using org.mariuszgromada.math.mxparser;
+
+namespace ValorAgregado
+{
+    class ReglaFalsa
+    {
+        static void Main(string[] args) {
+            string func;
+            double xi, xs, tolerancia;
+            int iteraciones;
+
+            Console.WriteLine("Convenciones usadas: pi, sin(), cos(), tan(), e^, abs(), ^, sqrt(), ln()");
+            Console.WriteLine("Ingrese la función f(x) a ser evaluada (Notación: f(x) = <expresión>)");
+            func = Console.ReadLine();
+            Console.WriteLine("La función es: " + func);
+
+            Console.WriteLine("Ingrese el extremo inferior: ");
+            xi = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el extremo superior: ");
+            xs = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la tolerancia: ");
+            tolerancia = Double.Parse(Console.ReadLine());
+            while (tolerancia == 0) {
+                Console.WriteLine("La tolerancia debe ser diferente de 0, ingresela nuevamente: ");
+                tolerancia = Double.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Ingrese el numero maximo de iteraciones: ");
+            iteraciones = int.Parse(Console.ReadLine());
+            while (iteraciones <= 0) {
+                Console.WriteLine("El numero de iteraciones debe ser mayor a 0, ingresela nuevamente: ");
+                iteraciones = int.Parse(Console.ReadLine());
+            }
+
+            metodoBiseccion(func, xi, xs, tolerancia, iteraciones);
+        }
+
+        public static void metodoBiseccion (string func, double xi, double xs, double tolerancia, int iteraciones) {
+            double fxi, fxs, fxm, xm, errorAbs, errorRel, xaux;
+            int cont;
+
+            Function fx = new Function(func);
+
+            fxi = fx.calculate(xi);
+            fxs = fx.calculate(xs);
+
+            if (fxi == 0) {
+                Console.WriteLine(xi + " es una raiz");
+            } else if (fxs == 0) {
+                Console.WriteLine(xs + " es una raiz");
+            } else if (fxi * fxs > 0) {
+                Console.WriteLine("El intervalo no contiene una raiz");
+            } else {
+                xm = (xi + xs)/2;
+                fxm = fx.calculate(xm);
+
+                Console.WriteLine("iteración | xi | fxi | xs | fxs | xm | fxm | error absoluto | error relativo");
+                Console.WriteLine(0 + "|" + xi + "|" + fxi + "|" + xs + "|" + fxs + "|" + xm + "|" + fxm);
+                cont = 1;
+                errorAbs = tolerancia + 1;
+                while (fxm != 0 && errorAbs > tolerancia && cont < iteraciones) {
+                    if (fxi * fxm < 0) {
+                        xs = xm;
+                        fxs = fx.calculate(xs);
+                    } else {
+                        xi = xm;
+                        fxi = fx.calculate(xi);
+                    }
+
+                    xaux = xm;
+                    xm = xi - (fxi*(xs-xi))/(fxs-fxi);
+                    fxm = fx.calculate(xm);
+                    errorAbs = Math.Abs(xm - xaux); 
+                    errorRel = errorAbs / xm;
+                    Console.WriteLine(cont + "|" + xi + "|" + fxi + "|" + xs + "|" + fxs + "|" + xm + "|" + fxm + "|" + errorAbs + "|" + errorRel);
+                    cont = cont + 1;
+                }
+
+                if (fxm == 0) {
+                    Console.WriteLine(xm + " es una raiz");
+                } else if (errorAbs < tolerancia) {
+                    Console.WriteLine(xm + " se aproxima a una raiz de la función, con una tolerancia de: " + tolerancia);
+                } else {
+                    Console.WriteLine("Excedió el numero de iteraciones posible");
+                }  
+            }
+        }
+    }
+}
